029c2bee990ee858edd151a1eec93e14ad59b8b5
Venkat Muthuswamy <venkat.muthuswamy@esgyn.com>
2015-09-10 19:02:57 -0700

    Fix query execution handling in Workbench and make the explain tooltip as popup dialog on click.

--- core/dbmgr/src/main/java/com/esgyn/dbmgr/resources/QueryResource.java
+++ core/dbmgr/src/main/java/com/esgyn/dbmgr/resources/QueryResource.java
@@ -2,8 +2,11 @@ package com.esgyn.dbmgr.resources;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -27,117 +30,136 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 
 @Path("/queries")
 public class QueryResource {
-  private static final Logger _LOG = LoggerFactory.getLogger(QueryResource.class);
+	private static final Logger _LOG = LoggerFactory.getLogger(QueryResource.class);
 
-  @POST
-  @Path("/execute/")
-  @Produces("application/json")
-  @Consumes("application/json")
-  public TabularResult executeQuery(ObjectNode obj, @Context HttpServletRequest servletRequest,
-      @Context HttpServletResponse servletResponse) throws EsgynDBMgrException {
+	@POST
+	@Path("/execute/")
+	@Produces("application/json")
+	@Consumes("application/json")
+	public TabularResult executeQuery(ObjectNode obj, @Context HttpServletRequest servletRequest,
+			@Context HttpServletResponse servletResponse) throws EsgynDBMgrException {
 
-    String queryText = obj.get("sQuery").textValue();
-    String sControlStmts = "";
-    if (obj.has("sControlStmts")) {
-      sControlStmts = obj.get("sControlStmts").textValue();
-    }
-    Session soc = SessionModel.getSession(servletRequest, servletResponse);
-    return executeSQLQuery(soc.getUsername(), soc.getPassword(), queryText, sControlStmts);
-  }
+		String queryText = obj.get("sQuery").textValue();
+		String sControlStmts = "";
+		if (obj.has("sControlStmts")) {
+			sControlStmts = obj.get("sControlStmts").textValue();
+		}
+		Session soc = SessionModel.getSession(servletRequest, servletResponse);
+		return executeSQLQuery(soc.getUsername(), soc.getPassword(), queryText, sControlStmts);
+	}
 
-  public static TabularResult executeSQLQuery(String user, String password, String queryText)
-      throws EsgynDBMgrException {
-    return executeSQLQuery(user, password, queryText, null);
-  }
+	public static TabularResult executeSQLQuery(String user, String password, String queryText)
+			throws EsgynDBMgrException {
+		return executeSQLQuery(user, password, queryText, null);
+	}
 
-  public static TabularResult executeSQLQuery(String user, String password, String queryText,
-      String sControlStmts) throws EsgynDBMgrException {
+	public static TabularResult executeSQLQuery(String user, String password, String queryText, String sControlStmts)
+			throws EsgynDBMgrException {
 
-    Connection connection = null;
-    Statement stmt;
-    ResultSet rs;
-    TabularResult js = new TabularResult();
+		Connection connection = null;
+		ResultSet rs;
+		TabularResult js = new TabularResult();
+		PreparedStatement pstmt = null;
 
-    String url = ConfigurationResource.getInstance().getJdbcUrl();
+		String url = ConfigurationResource.getInstance().getJdbcUrl();
 
-    try {
-      Class.forName(ConfigurationResource.getInstance().getJdbcDriverClass());
-    } catch (Exception e) {
-      e.printStackTrace();
-      System.out.println(e.getMessage());
-      System.exit(0);
-    }
+		try {
+			Class.forName(ConfigurationResource.getInstance().getJdbcDriverClass());
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println(e.getMessage());
+			System.exit(0);
+		}
 
-    try {
-      connection = DriverManager.getConnection(url, user, password);
+		try {
+			connection = DriverManager.getConnection(url, user, password);
 
-      String[] controlStatements = new String[0];
-      if (sControlStmts != null && sControlStmts.length() > 0) {
-        controlStatements = sControlStmts.split(";");
-      }
-      Statement stmt1 = connection.createStatement();
-      for (String controlText : controlStatements) {
-        stmt1.execute(controlText);
-      }
-      stmt1.close();
+			String[] controlStatements = new String[0];
+			if (sControlStmts != null && sControlStmts.length() > 0) {
+				controlStatements = sControlStmts.split(";");
+			}
+			Statement stmt1 = connection.createStatement();
+			for (String controlText : controlStatements) {
+				stmt1.execute(controlText);
+			}
+			stmt1.close();
 
-      stmt = connection.createStatement();
-      rs = stmt.executeQuery(queryText);
-      _LOG.debug(queryText);
-      js = Helper.convertResultSetToTabularResult(rs);
-      rs.close();
+			pstmt = connection.prepareStatement(queryText);
+			boolean hasResultSet = pstmt.execute();
+			_LOG.debug(queryText);
+			if (hasResultSet) {
+				rs = pstmt.getResultSet();
+				js = Helper.convertResultSetToTabularResult(rs);
+				rs.close();
+			} else {
+				int count = pstmt.getUpdateCount();
+				js.columnNames = new String[] { "Status" };
+				js.resultArray = new ArrayList<Object[]>();
+				Object[] data = new Object[1];
+				if (queryText.toLowerCase().startsWith("insert") || queryText.toLowerCase().startsWith("upsert")
+						|| queryText.toLowerCase().startsWith("update")
+						|| queryText.toLowerCase().startsWith("delete")) {
+					data[0] = String.format("%1$s row(s) affected", count);
+				} else {
+					data[0] = "The statement completed successfully.";
+				}
+				js.resultArray.add(data);
 
-    } catch (Exception e) {
-      _LOG.error("Failed to execute query : " + e.getMessage());
-      throw new EsgynDBMgrException(e.getMessage());
-    }
- finally {
-      if (connection != null) {
-        try {
-          connection.close();
-        } catch (Exception ex) {
+			}
+		} catch (Exception e) {
+			_LOG.error("Failed to execute query : " + e.getMessage());
+			throw new EsgynDBMgrException(e.getMessage());
+		} finally {
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
 
-        }
-      }
-    }
-    return js;
-  }
+				}
+			}
+			if (connection != null) {
+				try {
+					connection.close();
+				} catch (Exception ex) {
 
-  @POST
-  @Path("/explain/")
-  @Produces("application/json")
-  @Consumes("application/json")
-  public QueryPlanResponse getQueryPlan(ObjectNode obj, @Context HttpServletRequest servletRequest,
-      @Context HttpServletResponse servletResponse) throws EsgynDBMgrException {
+				}
+			}
+		}
+		return js;
+	}
 
-    Session soc = SessionModel.getSession(servletRequest, servletResponse);
+	@POST
+	@Path("/explain/")
+	@Produces("application/json")
+	@Consumes("application/json")
+	public QueryPlanResponse getQueryPlan(ObjectNode obj, @Context HttpServletRequest servletRequest,
+			@Context HttpServletResponse servletResponse) throws EsgynDBMgrException {
 
-    String queryText = obj.get("sQuery").textValue();
-    String sControlStmts = "";
-    String sQueryID = "";
+		Session soc = SessionModel.getSession(servletRequest, servletResponse);
 
-    if (obj.has("sControlStmts")) {
-      sControlStmts = obj.get("sControlStmts").textValue();
-    }
-    if (obj.has("sQueryID")) {
-      sQueryID = obj.get("sQueryID").textValue();
-    }
+		String queryText = obj.get("sQuery").textValue();
+		String sControlStmts = "";
+		String sQueryID = "";
 
-    try {
-      Class.forName(ConfigurationResource.getInstance().getJdbcDriverClass());
+		if (obj.has("sControlStmts")) {
+			sControlStmts = obj.get("sControlStmts").textValue();
+		}
+		if (obj.has("sQueryID")) {
+			sQueryID = obj.get("sQueryID").textValue();
+		}
 
-    } catch (Exception e) {
-      _LOG.error("Failed to load driver : " + e.getMessage());
-    }
+		try {
+			Class.forName(ConfigurationResource.getInstance().getJdbcDriverClass());
 
-    if (queryText == null || queryText.length() < 1) {
-      // queryText = "select [first 100] * from manageability.instance_repository.event_text_1";
-    }
-    QueryPlanModel qe = new QueryPlanModel();
-    qe.GeneratePlan(soc.getUsername(), soc.getPassword(), queryText, sControlStmts, sQueryID);
+		} catch (Exception e) {
+			_LOG.error("Failed to load driver : " + e.getMessage());
+		}
 
-    QueryPlanResponse response = qe.getQueryPlanResponse();
-    return response;
-  }
+		QueryPlanModel qe = new QueryPlanModel();
+		qe.GeneratePlan(soc.getUsername(), soc.getPassword(), queryText, sControlStmts, sQueryID);
+
+		QueryPlanResponse response = qe.getQueryPlanResponse();
+		return response;
+	}
 
 }
