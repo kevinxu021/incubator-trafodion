6712d43071f993db3421207cc72c85309ffa1157
Weiqing Xu <weiqing.xu@esgyn.cn>
2015-09-29 14:36:43 +0800

    remove some unused files
    1. remove unused project: EsgynDB.Data.VisualStudio and InstalShield
    project files.
    2. update the auto build script to use the inno setup.

--- core/conn/ado/build.bat
+++ core/conn/ado/build.bat
@@ -1,16 +1,22 @@
 @set build_dir=c:\Build
+@set INNO_SETUP_PATH=C:\Program Files (x86)\Inno Setup 5
+
+@set PATH=%INNO_SETUP_PATH%;%PATH%
 
 @devenv EsgynDBProvider.sln /clean
 @devenv EsgynDBProvider.sln /build "Release"
-@copy installer\installer\Express\SingleImage\DiskImages\DISK1\EsgynDB-ADO.NET-1.0.0.exe "%build_dir%"
 
-if not exist "%build_dir%\EsgynDB-ADO.NET-1.0.0.exe" goto failend
-@goto end
+@set PATH=%INNO_SETUP_PATH%;%PATH%
+
+if not exist "installer\Output\EsgynDB-ADO.NET-1.0.0.exe" goto failend
+
+ISCC.exe /Q installer\installer.iss
+
+@echo on
+@echo *** build successfully *** goto end
 
 :failend
 @echo on
 @echo *** build failend ***
 
-:end 
-@echo on
-@echo *** build successfully ***
+:end
