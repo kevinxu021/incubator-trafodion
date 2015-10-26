ff437f2a5293536d220ea4a9ad9fd73cf676c0f3
Venkat Muthuswamy <venkat.muthuswamy@esgyn.com>
2015-10-26 14:41:23 -0700

    Add system version and dbmgr version to about dialog

--- core/dbmgr/src/main/webapp/js/views/help/AboutView.js
+++ core/dbmgr/src/main/webapp/js/views/help/AboutView.js
@@ -8,19 +8,42 @@ define([
         'views/BaseView',
         'text!templates/about.html',
         'jquery',
-        'model/Localizer',
-        'moment',
-        'common',
-        'views/RefreshTimerView',
-        'jqueryui',
-        'jqueryvalidate'
-        ], function (BaseView, aboutT, $, localizer, moment, common, refreshTimerView) {
+        'handlers/ServerHandler'
+          ], function (BaseView, aboutT, $, serverHandler) {
 	'use strict';
+
+	var SYSTEM_VERSION = '#system-version',
+		DBMGR_VERSION = '#dbmgr-version',
+		SPINNER = '#loadingImg',
+		RESULT_CONTAINER = '#version-container',
+		ERROR_CONTAINER= '#errorText';
+
+	var _that = null;
 
 	var AboutView = BaseView.extend({
 		template:  _.template(aboutT),
 
-		doInit: function (){}
+		doInit: function (){
+			serverHandler.on(serverHandler.FETCH_VERSION_SUCCESS, this.displayResults);
+			serverHandler.on(serverHandler.FETCH_VERSION_ERROR, this.showErrorMessage);	
+			$(SPINNER).show();
+			serverHandler.fetchServerInfo();
+		},
+		displayResults: function(data){
+			$(SPINNER).hide();
+			$(ERROR_CONTAINER).hide();
+			$(RESULT_CONTAINER).show();
+			$(SYSTEM_VERSION).val(data.SYSTEM_VERSION);
+			$(DBMGR_VERSION).val(data.DBMGR_VERSION);
+		},
+		showErrorMessage: function (jqXHR) {
+			$(SPINNER).hide();
+			$(RESULT_CONTAINER).hide();
+			$(ERROR_CONTAINER).show();
+			if (jqXHR.responseText) {
+				$(ERROR_CONTAINER).text(jqXHR.responseText);
+			}
+		}  
 
 	});
 
