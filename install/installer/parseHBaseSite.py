840b78d7e583c7b022f96177f4a12fc06999c085
Amanda Moran <amanda@apache.com>
2016-02-22 20:20:32 +0000

    Mantis BUG 202

--- install/installer/parseHBaseSite.py
+++ install/installer/parseHBaseSite.py
@@ -1,5 +1,27 @@
 #!/usr/bin/python
 
+# @@@ START COPYRIGHT @@@
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+# @@@ END COPYRIGHT @@@
+
+
 import os
 
 import xml.etree.ElementTree as ET
@@ -9,6 +31,10 @@ hbaseRegion="hbase.regionserver.info.port"
 zooKeeperNodes="hbase.zookeeper.quorum"
 pathToHome= os.environ['HOME']
 
+hbaseMasterInfoPort="60010"
+regionServerInfoPort="60030"
+zookeeperNodeNames=""
+
 tree = ET.parse( pathToHome + '/hbase-site.xml')
 
 root = tree.getroot()
@@ -17,18 +43,14 @@ root = tree.getroot()
 for x in root.findall('property'):
     name = str(x.find('name').text)
     if name == hbaseMaster:
-       value = x.find('value').text
-       f = open( '/etc/trafodion/trafodion_config', 'a')
-       f.write ( 'export HBASE_MASTER_INFO_PORT="' + value + '"\n' )
-       f.close()
+       hbaseMasterInfoPort = x.find('value').text
     if name == hbaseRegion:
-       value = x.find('value').text
-       f = open( '/etc/trafodion/trafodion_config', 'a')
-       f.write ( 'export REGIONSERVER_INFO_PORT="' + value + '"\n' )
-       f.close()
+       regionServerInfoPort = x.find('value').text
     if name == zooKeeperNodes:
-       value = x.find('value').text
-       f = open( '/etc/trafodion/trafodion_config', 'a')
-       f.write ( 'export ZOOKEEPER_NODES="' + value + '"\n' )
-       f.close()
+       zookeeperNodeNames = x.find('value').text
 
+f = open( '/etc/trafodion/trafodion_config', 'a')
+f.write ( 'export HBASE_MASTER_INFO_PORT="' + hbaseMasterInfoPort + '"\n' )
+f.write ( 'export REGIONSERVER_INFO_PORT="' + regionServerInfoPort + '"\n' )
+f.write ( 'export ZOOKEEPER_NODES="' + zookeeperNodeNames + '"\n' )
+f.close()
