71fc3a35e2142cc94aec42e32250c23568eca02e
Kevin Xu <kaihua.xu@esgyn.cn>
2016-02-27 10:50:33 +0800

    fixed 'contains' issue Mantis-0000210

--- core/conn/ado/EsgynDB.Data/Public/EsgynDBParameterCollection.cs
+++ core/conn/ado/EsgynDB.Data/Public/EsgynDBParameterCollection.cs
@@ -191,7 +191,8 @@ namespace EsgynDB.Data
         /// <exception cref="InvalidCastException">The parameter passed was not a EsgynDBParameter.</exception>
         public override bool Contains(object value)
         {
-            return this._parameters.Exists(x => x.Value.Equals(((DbParameter)value).Value));
+            object tmpVal = ((DbParameter)value).Value;
+            return this._parameters.Exists(x => tmpVal == x.Value || (tmpVal != null && x.Value != null && x.Value.Equals(tmpVal)));
         }
 
         /// <summary>
