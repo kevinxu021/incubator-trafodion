473a7c26ff201fc6ae5a4471acb3b02dc6613a61
Hans Zeller <hans.zeller@esgyn.com>
2016-05-19 01:24:28 +0000

    Mantis-466 Invoke for partitioned Hive tables wrong
    It did not show partition columns.

--- core/sql/sqlcomp/CmpDescribe.cpp
+++ core/sql/sqlcomp/CmpDescribe.cpp
@@ -2331,7 +2331,7 @@ short CmpDescribeHiveTable (
     {
       NAColumn * nac = naTable->getNAColumnArray()[i];
 
-      if(nac->isHivePartColumn())
+      if(nac->isHivePartColumn() && type != 1)
           continue;
 
       if (!nac->isHiveVirtualColumn() || describeVirtCols)
