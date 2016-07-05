529c70b4d8f78a9c26baedea8292e1b156f04d90
william <weitaian1982@163.com>
2016-07-05 11:10:45 +0800

    revert

--- core/dbmgr/src/main/webapp/js/views/database/SchemasView.js
+++ core/dbmgr/src/main/webapp/js/views/database/SchemasView.js
@@ -100,7 +100,6 @@ define([
 				$.each(keys, function(k, v) {
 					var obj = new Object();
 					obj.title = v;
-					//obj.title = common.Translate(obj.title);
 					aoColumns.push(obj);
 				});
 
@@ -118,7 +117,8 @@ define([
 					"oLanguage": {
 						"sEmptyTable": "There are no schemas"
 					},
-					dom: '<"top"l<"clear">Bf>t<"bottom"rip>',
+					//dom: '<"top"l<"clear">Bf>t<"bottom"rip>',
+					dom: "<'row'<'col-md-8'lB><'col-md-4'f>>" +"<'row'<'col-md-12'<'datatable-scroll'tr>>><'row'<'col-md-12'ip>>",
 					processing: true,
 					paging: bPaging,
 					autoWidth: true,
@@ -168,15 +168,12 @@ define([
 						"searchable" : false
 					}],
 	                 buttons: [
-	                           { extend : 'copy', text: '复制', exportOptions: { columns: ':visible' } },
+	                           { extend : 'copy', exportOptions: { columns: ':visible' } },
 	                           { extend : 'csv', exportOptions: { columns: ':visible' } },
 	                          // { extend : 'excel', exportOptions: { columns: ':visible' } },
 	                           { extend : 'pdfHtml5', exportOptions: { columns: ':visible' }, title: 'Schemas' },
 	                           { extend : 'print', exportOptions: { columns: ':visible' }, title: 'Schemas' }
-	                           ],					             
-		             fnDrawCallback: function(){
-		            	 //$('#db-object-list-results td').css("white-space","nowrap");
-		             }
+	                           ]
 				});
 
 				$('#db-object-list-results tbody').on( 'click', 'td', function (e, a) {
