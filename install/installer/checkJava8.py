b3fed98ffbe9f1eec97d66449c29fd553e561ffc
Amanda Moran <amanda@apache.com>
2016-07-22 21:45:44 +0000

    JAVA VERSION CHECK CHANGE and small changes

---
+++ install/installer/checkJava8.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+
+import subprocess
+import re
+import os
+
+java8="1.8"
+
+pathToJava=os.environ["JAVA_HOME"]
+pathToJava = pathToJava + "/bin/" + "java"
+javaVersion  = [pathToJava, "-version"]
+
+
+
+sp = subprocess.Popen(javaVersion, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+lst_Java=list(sp.communicate())
+
+
+lst_Java=re.findall('version "[0-9.]+_[0-9]+"', lst_Java[1])
+
+
+if re.search(java8, str(lst_Java)):
+   os._exit(0)
+else:
+   os._exit(-1)
+
