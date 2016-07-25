a75bf86228b2ed9b4580d9d1229e98da93b1b498
Amanda Moran <amanda@apache.com>
2016-07-25 23:29:58 +0000

    More testing...

--- install/installer/checkJava8.py
+++ install/installer/checkJava8.py
@@ -17,9 +17,11 @@ sp = subprocess.Popen(javaVersion, stdout=subprocess.PIPE, stderr=subprocess.PIP
 
 lst_Java=list(sp.communicate())
 
+print lst_Java
 
 lst_Java=re.findall('version "[0-9.]+_[0-9]+"', lst_Java[1])
 
+print lst_Java
 
 if re.search(java8, str(lst_Java)):
    os._exit(0)
