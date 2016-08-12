23f9c630a7a84190bd92b1e12ea03e820343084b
root <root@vito-1.novalocal>
2016-08-12 22:54:44 +0800

    add bulkload tests

---
+++ perf_suites/JavaBench/sql/bulkload.lineitem.5.sql
@@ -0,0 +1,22 @@
+load with log error rows into LINEITEM
+select
+  L_ORDERKEY      
+, L_LINENUMBER    
+, L_PARTKEY       
+, L_SUPPKEY       
+, CAST(L_QUANTITY AS DECIMAL(15,2))      
+, CAST(L_EXTENDEDPRICE AS DECIMAL(15,2))      
+, CAST(L_DISCOUNT AS DECIMAL(15,2))      
+, CAST(L_TAX AS DECIMAL(15,2))        
+, L_RETURNFLAG    
+, L_LINESTATUS  
+, CAST(L_SHIPDATE AS DATE)     
+, CAST(L_COMMITDATE AS DATE)    
+, CAST(L_RECEIPTDATE AS DATE)       
+, L_SHIPINSTRUCT  
+, L_SHIPMODE      
+, L_COMMENT       
+from 
+hive.hive.LINEITEM_5
+; 
+
